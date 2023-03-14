afafda1c4c1b6e78ea8e12abd4757a54b9cba516
slothory <126821929+slothory@users.noreply.github.com>
2023-03-14 23:03:40 +0000

    Added comments and added the questions and their answers

---
+++ main.py
@@ -0,0 +1,4 @@
+#this is where the questions and their answers are stored
+questions = ["What is the main subject studied in B-block?", "What is the main subject studied in E-block?", "What is the main subject studied in H-block?", "What is the main subject studied in G-block?", "What is the main subject studied in C-block?", "What is the main subject studied in F-block?", "What is the main subject studied in A-block?", "What is the main subject studied in K-block?", "What is the main subject studied in the RHS gyms?", "What is the main subject studied in P-block?", ]
+answers = ["Tech", "Tech", "Food", "English", "Math", "Science", "Art", "Language", "P.E", "Science"]
+
